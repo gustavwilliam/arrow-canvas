05a7d4e0b0c24efaf71069c8b0e602c367f91340
Gustav Odinger <65498475+gustavwilliam@users.noreply.github.com>
2024-08-03 21:20:10 +0200

    Add header to selection menu

--- src/components/TheSelectionBar.vue
+++ src/components/TheSelectionBar.vue
@@ -1,21 +1,29 @@
 <template>
   <div class="absolute top-5 left-1/2 overflow-hidden -translate-x-1/2 bg-white border rounded-lg shadow-md z-50">
-    <div class="w-full h-full flex relative">
-      <ButtonOption name="Delete" @click="selectionStore.deleteLine"><TrashIcon/></ButtonOption>
-      <ToggleOptions>
-        <ToggleOptionRadio group="selection-tileset" value="default" name="Arrows" v-model:activeItem="selectionStore.line.tileset">
-          <div class="-mx-2 size-5 flex">
-            <img src="/tilesets/default/start-rr.svg" alt="Tileset start">
-            <img src="/tilesets/default/end-ll.svg" alt="Tileset end">
-          </div>
-        </ToggleOptionRadio>
-        <ToggleOptionRadio group="selection-tileset" value="lemoji" name="Lemoji" v-model:activeItem="selectionStore.line.tileset">
-          <div class="-mx-6 -mt-1 size-8 flex">
-            <img src="/tilesets/lemoji/start-rr.svg" alt="Tileset start">
-            <img src="/tilesets/lemoji/end-ll.svg" alt="Tileset end">
-          </div>
-        </ToggleOptionRadio>
-      </ToggleOptions>
+    <div class="bg-gray-100 py-3 px-4 flex justify-between">
+      <span class="font-semibold text-gray-900">1 line selected</span>
+      <button @click="selectionStore.clearSelection" class="bg-gray-200 hover:bg-gray-300 px-3 uppercase text-sm font-semibold text-gray-800 rounded-full">
+        Unselect
+      </button>
+    </div>
+    <div class="w-full">
+      <div class="w-full h-full flex relative">
+        <ButtonOption name="Delete" @click="selectionStore.deleteLine"><TrashIcon/></ButtonOption>
+        <ToggleOptions>
+          <ToggleOptionRadio group="selection-tileset" value="default" name="Arrows" v-model:activeItem="selectionStore.line.tileset">
+            <div class="-mx-2 size-5 flex">
+              <img src="/tilesets/default/start-rr.svg" alt="Tileset start">
+              <img src="/tilesets/default/end-ll.svg" alt="Tileset end">
+            </div>
+          </ToggleOptionRadio>
+          <ToggleOptionRadio group="selection-tileset" value="lemoji" name="Lemoji" v-model:activeItem="selectionStore.line.tileset">
+            <div class="-mx-6 -mt-1 size-8 flex">
+              <img src="/tilesets/lemoji/start-rr.svg" alt="Tileset start">
+              <img src="/tilesets/lemoji/end-ll.svg" alt="Tileset end">
+            </div>
+          </ToggleOptionRadio>
+        </ToggleOptions>
+      </div>
     </div>
   </div>
 </template>
