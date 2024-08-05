fbb638ef26a5eca2a7b67c566f172e8cdb718879
Gustav Odinger <65498475+gustavwilliam@users.noreply.github.com>
2024-08-05 11:03:20 +0200

    Fix history for lines deleted by selection

--- src/stores/selection.js
+++ src/stores/selection.js
@@ -1,15 +1,23 @@
 import { defineStore } from "pinia";
+import { useStateStore } from "./state";
 import { computed, ref } from "vue";
 
 export const useSelectionStore = defineStore("selection", () => {
   const line = ref(null);
   const point = ref(null);
+  const _stateStore = useStateStore();
 
   const hasSelection = computed(() => line.value || point.value);
 
   function deleteLine() {
     if (!line.value) return;
-    line.value.points.length = 0;
+    // create a new entry in history that's the same as the last item at the historyindex, but without the selected line
+    const newState = _stateStore
+      .currentLines()
+      .toReversed()
+      .filter((l) => l !== line.value);
+    _stateStore.history.push(newState);
+    _stateStore.historyIndex = 0;
     clearSelection();
   }
 
