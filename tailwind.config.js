a8d95547041dc8cb39a4eed230d28e7d5c78e067
Gustav Odinger <65498475+gustavwilliam@users.noreply.github.com>
2024-08-05 09:14:45 +0200

    Add shadcn

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,16 +1,43 @@
 /** @type {import('tailwindcss').Config} */
-export default {
-  purge: ["./index.html", "./src/**/*.{vue,js,ts,jsx,tsx}"],
-  content: [],
+module.exports = {
+  darkMode: ["class"],
+  content: [
+    "./pages/**/*.{js,jsx,vue}",
+    "./components/**/*.{js,jsx,vue}",
+    "./app/**/*.{js,jsx,vue}",
+    "./src/**/*.{js,jsx,vue}",
+  ],
+  prefix: "",
   theme: {
+    container: {
+      center: true,
+      padding: "2rem",
+      screens: {
+        "2xl": "1400px",
+      },
+    },
     extend: {
+      keyframes: {
+        "accordion-down": {
+          from: { height: 0 },
+          to: { height: "var(--radix-accordion-content-height)" },
+        },
+        "accordion-up": {
+          from: { height: "var(--radix-accordion-content-height)" },
+          to: { height: 0 },
+        },
+      },
+      animation: {
+        "accordion-down": "accordion-down 0.2s ease-out",
+        "accordion-up": "accordion-up 0.2s ease-out",
+      },
       translate: {
         "connector-arrows": "11%",
         "connector-lemoji": "25%",
       },
     },
   },
-  plugins: [],
+  plugins: ["animate"],
   safelist: [
     {
       pattern: /-?translate-(x|y)-connector-.+/,
