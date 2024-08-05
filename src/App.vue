786f93cf9fd8ff7892f158243ae7ee1bdab4c3d0
Gustav Odinger <65498475+gustavwilliam@users.noreply.github.com>
2024-08-05 11:12:40 +0200

    Add mobile unsupported message
    While mobile is not available, might as well tell the visitors that
    rather than presenting them with a broken view

--- src/App.vue
+++ src/App.vue
@@ -1,4 +1,8 @@
 <template>
+  <div class="z-[60] md:hidden bg-gray-100 p-10 text-center absolute inset-0 flex flex-col gap-2 items-center justify-center">
+    <div class="font-bold text-lg">Mobile view is not supported yet. Please use a desktop browser.</div>
+    <div class="text-sm text-gray-700">If you have a wide mobile device with a mouse, that would technically work too.</div>
+  </div>
   <div class="h-full w-full flex flex-col">
     <TheMenu />
     <div class="h-full w-full flex flex-col md:flex-row">
