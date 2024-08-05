fd16f7e76e9d0783cc42745ec3247b70598d0259
Gustav Odinger <65498475+gustavwilliam@users.noreply.github.com>
2024-08-05 10:54:52 +0200

    Uninstall shadcn

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,5 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  darkMode: ["class"],
   content: [
     "./pages/**/*.{js,jsx,vue}",
     "./components/**/*.{js,jsx,vue}",
@@ -9,35 +8,13 @@ module.exports = {
   ],
   prefix: "",
   theme: {
-    container: {
-      center: true,
-      padding: "2rem",
-      screens: {
-        "2xl": "1400px",
-      },
-    },
     extend: {
-      keyframes: {
-        "accordion-down": {
-          from: { height: 0 },
-          to: { height: "var(--radix-accordion-content-height)" },
-        },
-        "accordion-up": {
-          from: { height: "var(--radix-accordion-content-height)" },
-          to: { height: 0 },
-        },
-      },
-      animation: {
-        "accordion-down": "accordion-down 0.2s ease-out",
-        "accordion-up": "accordion-up 0.2s ease-out",
-      },
       translate: {
         "connector-arrows": "11%",
         "connector-lemoji": "25%",
       },
     },
   },
-  plugins: ["animate"],
   safelist: [
     {
       pattern: /-?translate-(x|y)-connector-.+/,
